5b822f12fbe9278f0961c3bc3d1a636b68ad9635
Joao Ceia <joao.p.ceia@gmail.com>
2022-02-11 16:56:07 +0000

    fixed init script

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+
+# Starting services
 service nginx start
 service mysql start
 service php7.3-fpm start
+
+# Checking services status
+nginx -t
+service nginx status
+service mysql status
+service php7.3-fpm status
 
 # ----------------------------------------------------------------------------
 # CONFIGURE A WORDPRESS DATABASE
@@ -10,7 +19,7 @@ service php7.3-fpm start
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
 
 # Grant privileges to user 'root'
-echo "GRAND ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
 
 # Reload the grant tables
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
