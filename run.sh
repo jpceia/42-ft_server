3aaa0e92704a4cfcb83f34fcf06e9ffd3e073d71
Joao Ceia <joao.p.ceia@gmail.com>
2022-02-12 19:34:28 +0000

    autoindex on by default

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name ft_server_container -e AUTOINDEX='off' -it -p 80:80 -p 443:443 ft_server
+docker run --rm --name ft_server_container -e AUTOINDEX='on' -it -p 80:80 -p 443:443 ft_server
